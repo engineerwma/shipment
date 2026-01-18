b4cc504aa211b17b7a89bee27539731a210f5e2b
Waleed Abboud <Waleed.Abboud@giglt.com.eg>
2026-01-18 10:56:24 +0200

    first commit

--- src/app/admin/drivers/[id]/page.tsx
+++ src/app/admin/drivers/[id]/page.tsx
@@ -232,12 +232,12 @@ export default function DriverDetailsPage() {
             </div>
           </div>
         </div>
-      </DashboardLayout>
+      </>
     );
   }
 
   return (
-    <DashboardLayout>
+    <>
       <div className="space-y-6">
         {/* Header */}
         <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-4">
